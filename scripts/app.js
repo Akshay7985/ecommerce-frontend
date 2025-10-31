ea33384872879f80006fb9382e650724049ff916
Akshay7985 <akshay21101@iiitnr.edu.in>
2025-10-31 16:18:46 +0530

    Initial project setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
 console.log("E-Commerce Website Loaded");
+
+// Select elements
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+// Toggle navigation menu on mobile
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+//Add Smooth Scroll for the Button
+const ctaButton = document.querySelector(".cta-btn");
+
+ctaButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 700, // Adjust this value later when you add sections
+    behavior: "smooth"
+  });
+});
